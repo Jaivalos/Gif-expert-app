8005c6b650b25c60c56e81f73a56f8a8e748c37b
Javier Avalos <jaivalos18j2@gmail.com>
2021-12-02 22:29:24 -0600

    List and button added

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const GifExpertApp = () =>{
+    // const cat = ['Guard', 'Shooting guard', 'Center'];
+
+    const [cat, setCategories] = useState(['Guard', 'Shooting guard', 'Center'])
+
+    const handleAdd = () =>{
+        setCategories([...cat, 'Forward']);
+    }
+
     return(
         <div>
-            <h2>GifExpertApp</h2>
-            <hr></hr>
+            <header>
+                <h2>GifExpertApp</h2>
+                <hr />
+            </header>
+
+            <button onClick = { handleAdd }>Agregar</button>
+
+            <ol>
+                {
+                    cat.map( (category) =>{
+                        return <li key={category}> {category} </li>
+                    } )
+                }
+            </ol>
+
         </div>
     );
 }

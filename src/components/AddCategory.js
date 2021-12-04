cbc95b843c2203e2136eee8faccde1e0ae0342ca
Javier Avalos <jaivalos18j2@gmail.com>
2021-12-04 09:26:09 -0600

    Se realiza la entrada del input con la tecla enter

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AddCategory = () => {
+
+    const [inputValue, setinputValue] = useState("Hola mundo")
+     
+    const handleInputChange = (e)=>{
+        setinputValue(e.target.value);
+    }
+
+    const handleSubmit = (e) =>{
+        console.log("Submit hecho");
+        e.preventDefault();
+    }
+
     return (
-        <>
-            <h2>AddCategory</h2>
-        </>
+        <form onSubmit={ handleSubmit }>
+            <input 
+                type="text"
+                value={ inputValue }
+                onChange= { handleInputChange }
+            />
+        </form>
     )
 }
--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,22 +1,22 @@
 import React, {useState} from 'react';
+import { AddCategory } from './AddCategory';
 
 export const GifExpertApp = () =>{
     // const cat = ['Guard', 'Shooting guard', 'Center'];
 
     const [cat, setCategories] = useState(['Guard', 'Shooting guard', 'Center'])
 
-    const handleAdd = () =>{
-        setCategories([...cat, 'Forward']);
-    }
+    // const handleAdd = () =>{
+    //     setCategories([...cat, 'Forward']);
+    // }
 
     return(
         <div>
             <header>
-                <h2>GifExpertApp</h2>
+                <h2 className="title">GifExpertApp</h2>
+                <AddCategory />
                 <hr />
             </header>
-
-            <button onClick = { handleAdd }>Agregar</button>
 
             <ol>
                 {

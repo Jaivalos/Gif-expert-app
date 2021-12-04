284a90c0fc34575bd3c9fc14943619fa064b66be
Javier Avalos <jaivalos18j2@gmail.com>
2021-12-04 10:24:14 -0600

    Ejemplo del useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export const GifGrid = ( {cat} ) => {
+
+    const [cont, setcont] = useState(0)
+
+    useEffect( () => {
+        getGif();
+    }, [])
 
     const getGif = async()=>{
         const url =' https://api.giphy.com/v1/gifs/search?q=Michael Jordan&limit=5&api_key=SBqbOdBrevdh1CiR0lFggV3dum7Qa7fT'
@@ -19,11 +25,11 @@ export const GifGrid = ( {cat} ) => {
 
     }
 
-    getGif();
-
     return (
         <>
             <h3>{cat}</h3>
+            <h3>{cont}</h3>
+            <button onClick={ ()=> setcont(cont + 1) }></button>
         </>
     )
 }

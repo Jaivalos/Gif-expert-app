6df0fd9dea5455bd8902f750ade0c05299546b66
Javier Avalos <jaivalos18j2@gmail.com>
2021-12-06 21:14:09 -0600

    Helper created and don

---
+++ src/helpers/GetGif.js
@@ -0,0 +1,18 @@
+
+
+export const getGif = async(category)=>{
+    const url =`https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=SBqbOdBrevdh1CiR0lFggV3dum7Qa7fT`
+    const resp = await fetch( url );
+    const {data} = await resp.json();
+
+    const gifs = data.map(img=>{
+        return{
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+        }
+    })
+
+    return gifs;
+
+}
